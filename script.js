d8b11a9a29b02a42716ebe8a9242d3802f0b36a9
issist <tamerlan.issay@yahoo.com>
2023-02-16 21:08:21 +0500

    finished my first challenge

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const button = document.querySelector('.btn');
 const share = document.querySelector('.pop');
+const image = document.querySelector('.image');
 
 button.addEventListener('click', () => {
+    if (share.classList.contains('hidden')){
+        image.style.backgroundColor = 'hsl(214, 17%, 51%)';
+        image.src = './images/icon-share-active.svg';
+    }else{        
+        image.style.backgroundColor = 'hsl(210, 46%, 95%)';
+        image.src = './images/icon-share.svg'
+    }
     share.classList.toggle('hidden');
+        
+
 });
